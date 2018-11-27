7d7b7399acca30a803ff11164b1995a125fbfe7f
Nathan Faltermeier <nefalt2@gmail.com>
2018-11-26 21:23:36 -0600

    Add ace to card number list

--- src/main/java/solitaire/graphics/ImageLoader.java
+++ src/main/java/solitaire/graphics/ImageLoader.java
@@ -15,18 +15,17 @@ public class ImageLoader {
         int cardTexWidth = 40; //2.5 * 16
         int cardTexHeight = 56; //3.5 * 16
 
+        // Max- Here's how i thought about loading the images into the program
+        String extension = ".png";
+        String bridging = "_of_";
+        String[] suitNames = {"clubs", "diamonds", "hearts", "spades"};
+        String[] numberNames = {"ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"};
+
+        // Change^^  Lets make a function that returns the image based upon the ID of the card passed in
+
         for(int i=1;i<=cardTextures.length;i++) {
             //Create a BufferedImage for each card's texture
         }
-
-        //Max- Here's how i thought about loading the images into the program
-        // String pngString = ".png";
-        // String ofString = "_of_";
-        // String[] suitNames = {"clubs", "diamonds", "hearts", "spades"};
-       // String[] numberNames = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"};
-
-       //Change^^  Lets make a function that returns the image based upon the ID of the card passed in
-   
 
         // Create a BufferedImage for the texture of the back of each card
         // Create a BufferedImage for the texture of a stack when it's empty
